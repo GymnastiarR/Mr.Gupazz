12ec7664235cf9de7bb0ee6eb432cf622285ef04
=GymnastiarR <=>
2024-03-31 00:34:34 +0700

    feat/completing

---
+++ app/src/main/java/com/example/mrgupazz/CompletingActivity.java
@@ -0,0 +1,14 @@
+package com.example.mrgupazz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompletingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_completing);
+    }
+}
