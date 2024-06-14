586df463832e41bcda463351c5f579041cf542a4
Fajar Rizki Ramadhan <2110511029@mahasiswa.upnvj.ac.id>
2024-06-14 19:04:31 +0700

    learning

--- app/src/main/java/com/example/mrgupazz/api/learning/learning.java
+++ app/src/main/java/com/example/mrgupazz/api/learning/learning.java
@@ -3,9 +3,13 @@ package com.example.mrgupazz.api.learning;
 public class learning {
     private String learning;
 
-    public String getLearning() {
-        return learning;
-    }
+    public void getLearning(){
+        getLearning();
+    };
+
+    public String setLearning(alphabet){
+      setLearning(alphabet)
+    };
 
 
 
